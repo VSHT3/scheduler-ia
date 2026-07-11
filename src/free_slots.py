5bb1f3957cd925540452e633c4a3b513fca8a8bc
VSHT3 <alexstefko50@gmail.com>
2026-07-11 12:02:16 +0200

    mulitple rewrites of free_slots.py.
    Current version is working, except a few edgecases

--- src/free_slots.py
+++ src/free_slots.py
@@ -3,70 +3,54 @@ from datetime import date, time, datetime
 
 
 day = date(2027,7,12)
-events = [
-    {
-        "name": "School",
-        "start": datetime(2027, 7, 12, 8, 0),
-        "end": datetime(2027, 7, 12, 15, 0),
-        "repeat": "none",
-    },
-    {
-        "name": "School",
-        "start": datetime(2027, 7, 12, 8, 0),
-        "end": datetime(2027, 7, 12, 17, 0),
-        "repeat": "none",
-    },
-    {
-        "name": "School",
-        "start": datetime(2027, 7, 12, 8, 0),
-        "end": datetime(2027, 7, 12, 22, 0),
-        "repeat": "none",
-    },
-    {
-        "name": "Hockey practice",
-        "start": datetime(2027, 7, 12, 16, 0),
-        "end": datetime(2027, 7, 12, 18, 0),
-        "repeat": "none",
-    },
-    {
-        "name": "Dinner",
-        "start": datetime(2027, 7, 12, 16, 0),
-        "end": datetime(2027, 7, 12, 19, 15),
-        "repeat": "none",
-    },
-]
+events = []
 
 
 config = {
     "workstart": time(7, 0),
-    "workend": time(22, 0),
+    "workend": time(23, 0),
     "daily_cap": 300,
     "horizon": 7,
     "heuristic": "edf",
 }
+
+
+
 block_start = datetime.combine(day, config["workstart"])
 block_end = datetime.combine(day, config["workend"])
 
 cursor=block_start
 sorted_events = sorted(events, key=lambda event: event["start"]) # sorted list orders things based on asc start
+number_of_events = len(sorted_events)
 
 free_slots=[]
-previous_end=datetime(1,1,1,1,1)
-for sorted_event in sorted_events:
-    if  block_start < sorted_event["start"] and sorted_event["end"] < block_end: # big blocker, ignore for now
-        break
-    elif cursor < sorted_event["start"]:
-        if sorted_event["start"] < previous_end and sorted_event["end"] > previous_end:
-            free_slots.pop()
+if events == []:
+    free_slots.append((block_start,block_end))
+else:
+    for index, sorted_event in enumerate(sorted_events):
+        if sorted_event["start"] <= block_start and block_end <= sorted_event["end"]: # big blocker, ignore for now
+            print("huge event, full day")
+            break # return empty free_slots
+        elif block_end <= sorted_event["end"]: # checks if an event ends after blockend
             free_slots.append((cursor,sorted_event["start"]))
+            print("day is finished")
+            break
+        elif index == (number_of_events-1):
+            if cursor < sorted_event["start"]:
+                free_slots.append((cursor,sorted_event["start"]))
+                free_slots.append((sorted_event["end"],block_end))
+        elif sorted_event["start"] <= cursor: # checks if event start <=cursor
+            if sorted_event["end"] > cursor: # the "stairs" problem
+                cursor=sorted_event["end"]
+            else: # the "smaller event within a bigger event" problem
+                print("smaller event within a bigger event is ignored, just to be sure")
+                pass
+                #ignore
         else:
-            previous_start=sorted_event["start"]
-            previous_end=sorted_event["end"]
             free_slots.append((cursor,sorted_event["start"]))
             cursor=sorted_event["end"]
-    else:
-        pass
 
+print(free_slots)
 
 
 
