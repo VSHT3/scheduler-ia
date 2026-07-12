a16baeaba01722a297e3862ed4ffb58168216a9e
VSHT3 <alexstefko50@gmail.com>
2026-07-13 00:58:03 +0200

    updated freeslots.py
    make_horizon_freeslots is functional
    moved split_events from make day to make horizon to prevent wasteful
    looping
    fixed splitters self mutability

--- src/freeslots.py
+++ src/freeslots.py
@@ -5,20 +5,21 @@ from datetime import time, datetime, timedelta
 def split_events(events):
     split_events=[]
     for event in events:
-        if event["start"].date() != event["end"].date():
+        current = event["start"]
+        if current.date() != event["end"].date():
             split_event={
                 "name": event["name"],
-                "start": event["start"],
-                "end": datetime.combine(event["start"].date(), time(23,59,59)),
+                "start": current,
+                "end": datetime.combine(current.date(), time(23,59,59)),
                 "repeat": event["repeat"],
             }
             split_events.append(split_event)
-            while event["start"].date() != event["end"].date():
-                event["start"] = event["start"] + timedelta(days=1) # increment event[start]
-                if event["start"].date() == event["end"].date(): # this is the last day of the split
+            while current.date() != event["end"].date():
+                current += timedelta(days=1) # increment event[start]
+                if current.date() == event["end"].date(): # this is the last day of the split
                     split_event={
                         "name": event["name"],
-                        "start": datetime.combine(event["start"].date(), time(0,0)),
+                        "start": datetime.combine(current.date(), time(0,0)),
                         "end": event["end"],
                         "repeat": event["repeat"],
                     }
@@ -26,8 +27,8 @@ def split_events(events):
                 else:
                     split_event={
                         "name": event["name"],
-                        "start": datetime.combine(event["start"].date(), time(0,0)),
-                        "end": datetime.combine(event["start"].date(), time(23,59,59)),
+                        "start": datetime.combine(current.date(), time(0,0)),
+                        "end": datetime.combine(current.date(), time(23,59,59)),
                         "repeat": event["repeat"],
                     }
                     split_events.append(split_event)
@@ -41,7 +42,6 @@ def make_day_freeslots(today, events, config):
     block_start = datetime.combine(today, config["workstart"])
     block_end = datetime.combine(today, config["workend"])
     cursor=block_start
-    events = split_events(events)
     sorted_events = sorted(events, key=lambda event: event["start"]) # sorted list orders things based on asc start
     ignore_last_event = False
     free_slots=[]
@@ -69,3 +69,10 @@ def make_day_freeslots(today, events, config):
 
 
 def make_horizon_freeslots(today,horizon,events,config):
+    split_events_list = split_events(events)
+    freeslots=[]
+    for i in range(horizon):
+        horizon_day = today + timedelta(days=i)
+        day_freeslots = make_day_freeslots(horizon_day,split_events_list,config)
+        freeslots.append(day_freeslots)
+    return freeslots
